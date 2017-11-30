801996b73be1b0df8838d06b3500993addf26c72
Andrea Wilkowski <andreawilkowski@yahoo.com>
2017-11-30 13:24:54 -0800

    Add branching and for each loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
-function Order(toppings, size) {
+function Order(toppings, size, price) {
   this.toppings = toppings;
   this.size = size;
+  this.price = price;
 }
-
 
 Order.prototype.priceCalc = function() {
-  if (this.size === small) {
+  if (this.size === "Small") {
     price = 10;
-  } else if (this.size === medium) {
+  } else if (this.size === "Medium") {
     price = 15;
   } else {
-    price = 20;
+    price = 80;
   }
-  if (this.time === "Matinee") {
-    price -= 5;
-  }
+  //if (this.time === "Matinee") {
+  //  price -= 5;
+//  }
   return price;
 }
+
 
 
 
@@ -26,15 +27,22 @@ $(document).ready(function(){
     event.preventDefault();
     $("#pizza-order").show();
     $("input:checkbox[name=pizza-toppings]:checked").each(function(){
-      var toppingsForPizza = $(this).val();
-      $('#pizza-order').append(toppingsForPizza + "<br>");
-    });
-    $("#pizza-size").show();
-    $("input:checkbox[name=sizes]:checked").each(function(){
-      var whatSizePizza = $(this).val();
-      $('#pizza-size').append(whatSizePizza + "<br>");
-    });
+      var toppings = $(this).val();
+      $('#pizza-order').append(toppings + "<br>");
 
+
+    $("#pizza-size").show();
+
+      var size = $("#size").val();
+      $('#pizza-size').append(size);
+
+      var price = 0;
+      var myOrder = new Order(toppings, size, price);
+
+      $("#pizza-cost").append(myOrder.priceCalc(price));
+      $("#pizza-cost").show()
+      console.log(myOrder.priceCalc(price));
+    });
     $('#order-form').hide();
   });
 });
